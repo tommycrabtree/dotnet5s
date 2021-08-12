449407135b7893df9ebc17d6ea7beec63b7e2224
Tommy <tommycrabtree@outlook.com>
2021-08-12 04:28:02 -0500

    End of Section 8

--- api/Extensions/ApplicationServiceExtensions.cs
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Helpers;
 using api.Interfaces;
 using api.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,8 @@ namespace api.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
---
+++ api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using api.DTOs;
+using api.Entities;
+using api.Extensions;
+using AutoMapper;
+
+namespace api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
+                    src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
