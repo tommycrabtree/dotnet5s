b9c369ab1b261c8e1318f2aef70706f9b2d5728d
Tommy <tommycrabtree@outlook.com>
2021-09-04 01:10:44 -0500

    End of Section 14

--- api/Entities/AppUser.cs
+++ api/Entities/AppUser.cs
@@ -21,10 +21,7 @@ namespace api.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; } = new List<Photo>();
-
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<UserLike> LikedByUsers { get; set; }
     }
 }
