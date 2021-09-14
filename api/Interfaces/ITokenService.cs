3e10e4ce789bfaaa4268dbc003caf69e1e752211
Tommy <tommycrabtree@outlook.com>
2021-09-14 03:21:09 -0500

    End of Section 16

--- api/Interfaces/ITokenService.cs
+++ api/Interfaces/ITokenService.cs
@@ -1,9 +1,10 @@
+using System.Threading.Tasks;
 using api.Entities;
 
 namespace api.Interfaces
 {
     public interface ITokenService
     {
-         string CreateToken(AppUser user);
+         Task<string> CreateToken(AppUser user);
     }
 }
