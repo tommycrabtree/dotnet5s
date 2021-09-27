73f95941a9cfcb6c25a3dcc50d2fb10379133afe
Tommy <tommycrabtree@outlook.com>
2021-09-27 01:50:35 -0500

    End of Section 18

--- api/DTOs/MessageDto.cs
+++ api/DTOs/MessageDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace api.DTOs
 {
@@ -14,5 +15,11 @@ namespace api.DTOs
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
         public DateTime MessageSent { get; set; }
+
+        [JsonIgnore]
+        public bool SenderDeleted { get; set; }
+
+        [JsonIgnore]
+        public bool RecipientDeleted { get; set; }
     }
 }
